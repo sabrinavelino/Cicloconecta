e6dab3890b3b5340205c53832cce815190f5ff7d
unknown <mmakarios44@gmail.com>
2025-07-09 23:06:44 -0300

    aa

--- cicloconecta/urls.py
+++ cicloconecta/urls.py
@@ -1,22 +1,15 @@
-"""
-URL configuration for cicloconecta project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.main, name='main'),
+    path('gabinete/', views.gabinete, name='gabinete'),
+    path('hd/', views.hd, name='hd'),
+    path('memoria/', views.memoria, name='memoria'),
+    path('meus-anuncios/', views.meus_anuncios, name='meus_anuncios'),
+    path('novo-anuncio/', views.novo_anuncio, name='novo_anuncio'),
+    path('usuarios/', include('usuarios.urls')),  
 ]
---
+++ cicloconecta/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+def main(request):
+    return render(request, 'main.html')
+
+def gabinete(request):
+    return render(request, 'gabinete.html')
+
+def hd(request):
+    return render(request, 'hd.html')
+
+def memoria(request):
+    return render(request, 'memoria.html')
+
+def meus_anuncios(request):
+    return render(request, 'meus-anuncios.html')
+
+def novo_anuncio(request):
+    return render(request, 'novo-anuncio.html')
